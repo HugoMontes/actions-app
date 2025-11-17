1f2e4cc21423f20ec4df8912f0481c7deccdc585
hmontes <hugomontes927@gmail.com>
2025-11-17 10:51:53 -0400

    Despliegue

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,6 @@ describe("GET /", () => {
   it("should return HTML", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toContain("Curso de GitHub Actions xxx");
+    expect(res.text).toContain("Curso de GitHub Actions");
   });
 });
