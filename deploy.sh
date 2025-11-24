ed18da38f348b6f60da7b35a15f1f12b8b8ef93d
hmontes <hugomontes927@gmail.com>
2025-11-23 20:55:59 -0400

    Despliegue

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ if [ "$BRANCH" == "main" ]; then
     DEPLOY_PATH="/home/azureuser/practicas-ga"
 else
     DEPLOY_PATH="/home/azureuser/stage"
+fi
 
-
-ssh -o StrictHostKeyChecking=no azureuser@4.227.181.213 << 'EOF'
+ssh -o StrictHostKeyChecking=no azureuser@4.227.181.213 << EOF
 
     cd $DEPLOY_PATH
 
